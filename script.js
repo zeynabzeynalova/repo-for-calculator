f2e8b469214d545e48518eb26564affd7fd0264a
zeynab <zzeynab694@gmail.com>
2025-03-17 00:08:08 +0400

    sss

--- script.js
+++ script.js
@@ -19,3 +19,21 @@ function calculateResult() {
         document.getElementById("display").value = "Error";
     }
 }
+function speakResult(text) {
+    let speech = new SpeechSynthesisUtterance(text);
+    speech.lang = "az-AZ"; // Azərbaycan dili
+    window.speechSynthesis.speak(speech);
+}
+
+function calculate() {
+    try {
+        let result = eval(document.getElementById("display").value);
+        document.getElementById("display").value = result;
+        playSound("success");
+        speakResult("Cavab " + result);
+    } catch {
+        document.getElementById("display").value = "Error";
+        playSound("error");
+        speakResult("Səhv baş verdi.");
+    }
+}
